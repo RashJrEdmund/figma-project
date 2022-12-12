7a40c4659d1266180fa71cf4cdccc0e6aa6ff4e1
rash <orashusedmund@gmail.com>
2022-12-12 08:57:15 +0100

    .

---
+++ assets/scripts/script.js
@@ -0,0 +1,5 @@
+const menuBtn = document.querySelector('.menu')
+const varyingDiv = document.querySelector('.varying')
+menuBtn.addEventListener('click', ()=>{
+    varyingDiv.classList.toggle('varying')
+})
